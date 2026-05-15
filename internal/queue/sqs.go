5cd2fcc12fa9bea01340f37c516be1ee0cedaa09
Suthar345Piyush <piyushsuthar524@gmail.com>
2026-05-16 01:42:54 +0530

    aws sqs completed

--- internal/queue/sqs.go
+++ internal/queue/sqs.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 	appconfig "github.com/suthar345piyush/internal/config"
 	"github.com/suthar345piyush/internal/logger"
 	"go.uber.org/zap"
@@ -118,4 +119,116 @@ func (c *Client) PublishWithDelay(ctx context.Context, msg WebhookMessage, delay
 
 	return nil
 
+}
+
+// function to send the messages after the retry limit exhausted into DLQ of SQS
+
+func (c *Client) SendToDLQ(ctx context.Context, msg WebhookMessage) error {
+
+	body, err := json.Marshal(msg)
+
+	if err != nil {
+		return fmt.Errorf("queue: marshal DLQ message: %w", err)
+	}
+
+	_, err = c.svc.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    aws.String(c.dlqURL),
+		MessageBody: aws.String(string(body)),
+	})
+
+	if err != nil {
+		return fmt.Errorf("queue: send message to DLQ: %w", err)
+	}
+
+	logger.Log.Warn("message moved to DLQ - all retries exhausted",
+
+		zap.String("delivery_id", msg.DeliveryID),
+		zap.String("webhook_id", msg.WebhookID),
+		zap.Int("attempt", msg.Attempt),
+	)
+
+	return nil
+
+}
+
+//consume function - it will read messages from sqs and call handler for each one of them
+// it basically a long Poll function
+
+func (c *Client) Consume(ctx context.Context, handler func(ctx context.Context, msg WebhookMessage) error) {
+
+	logger.Log.Info("SQS consumer started", zap.String("queue", c.queueURL))
+
+	// if their is an context cancellation between polls
+
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Log.Info("SQS consumer stopped")
+			return
+		default:
+		}
+
+		output, err := c.svc.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
+			QueueUrl: aws.String(c.queueURL),
+
+			MaxNumberOfMessages:         10,
+			WaitTimeSeconds:             20,
+			VisibilityTimeout:           30,
+			MessageSystemAttributeNames: []types.MessageSystemAttributeName{types.MessageSystemAttributeNameAll},
+		})
+
+		if err != nil {
+			// context cancelled - exit safely
+			if ctx.Err() != nil {
+				return
+			}
+
+			logger.Log.Error("SQS ReceiveMessage failed", zap.Error(err))
+			continue
+		}
+
+		for _, sqsMsg := range output.Messages {
+			sqsMsg := sqsMsg
+
+			var msg WebhookMessage
+
+			if err := json.Unmarshal([]byte(aws.ToString(sqsMsg.Body)), &msg); err != nil {
+
+				logger.Log.Error("failed to unmarshal SQS message body", zap.Error(err),
+					zap.String("body", aws.ToString(sqsMsg.Body)),
+				)
+
+				c.deleteMessage(ctx, sqsMsg.ReceiptHandle)
+				continue
+			}
+
+			if err := handler(ctx, msg); err != nil {
+				logger.Log.Error("handler failed - message will become visible again", zap.Error(err), zap.String("delivery_id", msg.DeliveryID))
+
+				continue
+			}
+
+			// handler succeeded - remove from the queue
+
+			c.deleteMessage(ctx, sqsMsg.ReceiptHandle)
+
+		}
+
+	}
+
+}
+
+// delete message function
+// receipt handle with the message which going to delete
+
+func (c *Client) deleteMessage(ctx context.Context, receiptHandle *string) {
+
+	_, err := c.svc.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(c.queueURL),
+		ReceiptHandle: receiptHandle,
+	})
+
+	if err != nil {
+		logger.Log.Error("failed to delete SQS message", zap.Error(err))
+	}
 }
