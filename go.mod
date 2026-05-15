6f6042a37e6ffc80f4b791880cad306ea731ce5c
Suthar345Piyush <piyushsuthar524@gmail.com>
2026-05-15 20:37:17 +0530

    setting up sqs standard queue for publishing and consuming messages

--- go.mod
+++ go.mod
@@ -2,9 +2,29 @@ module github.com/suthar345piyush
 
 go 1.26.2
 
+require github.com/aws/aws-sdk-go-v2/service/sqs v1.42.27
+
+require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.19.16 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/signin v1.0.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.30.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.21 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.42.1 // indirect
+)
+
 require (
 	github.com/andybalholm/brotli v1.2.1 // indirect
 	github.com/aws/aws-sdk-go v1.55.8 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.41.7 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.32.17
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/appconfig v1.43.15
+	github.com/aws/smithy-go v1.25.1 // indirect
 	github.com/gofiber/fiber/v3 v3.2.0 // indirect
 	github.com/gofiber/schema v1.7.1 // indirect
 	github.com/gofiber/utils/v2 v2.0.4 // indirect
--- internal/queue/sqs.go
+++ internal/queue/sqs.go
@@ -1,4 +1,121 @@
-// AWS - SQS - for producing and consuming messages  
+// AWS - SQS - for publishing and consuming messages
 
+package queue
 
- 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	appconfig "github.com/suthar345piyush/internal/config"
+	"github.com/suthar345piyush/internal/logger"
+	"go.uber.org/zap"
+)
+
+// webhook message is payload that we publish to sqs, when webhook event arrives
+// the worker will deserialises this to know what to deliver
+// it will be json payload
+
+type WebhookMessage struct {
+	DeliveryID string `json:"delivery_id"` // X-Github-Delivery header value
+	WebhookID  string `json:"webhook_id"`
+	EventType  string `json:"event_type"` // push, pull req, etc..
+	Attempt    int    `json:"attempt"`    // this will start with 1, increment on retry
+	Payload    []byte `json:"payload"`
+}
+
+// client will wraps the aws sqs sdk with our application queue operation
+
+type Client struct {
+	svc      *sqs.Client
+	queueURL string // main queue url
+	dlqURL   string // dead letter queue url
+}
+
+// now function to create sqs client using env vars and config
+
+func New(cfg *appconfig.Config) (*Client, error) {
+
+	awsCfg, err := config.LoadDefaultConfig(
+		context.Background(),
+		config.WithRegion(cfg.AWSRegion),
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("queue: load aws config: %w", err)
+	}
+
+	return &Client{
+		svc:      sqs.NewFromConfig(awsCfg),
+		queueURL: cfg.SQSQueueURL,
+		dlqURL:   cfg.SQSDLQUrl,
+	}, nil
+
+}
+
+// publish function to send message to main delivery queue
+
+func (c *Client) Publish(ctx context.Context, msg WebhookMessage) error {
+
+	// marshal the body (msg), serialize it
+
+	body, err := json.Marshal(msg)
+
+	if err != nil {
+		return fmt.Errorf("queue: marshal message: %w", err)
+	}
+
+	// using standard queues
+
+	_, err = c.svc.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    aws.String(c.queueURL),
+		MessageBody: aws.String(string(body)),
+	})
+
+	if err != nil {
+		return fmt.Errorf("queue: send message: %w", err)
+	}
+
+	logger.Log.Info("message published to SQS",
+		zap.String("delivery_id", msg.DeliveryID),
+		zap.String("event_type", msg.EventType),
+		zap.Int("attempt", msg.Attempt),
+	)
+
+	return nil
+
+}
+
+// publish message with some delay to main queue  with sqs delivery delay maximum of 900, the message will not be visible to consumer until delay over
+
+func (c *Client) PublishWithDelay(ctx context.Context, msg WebhookMessage, delaySec int) error {
+
+	body, err := json.Marshal(msg)
+
+	if err != nil {
+		return fmt.Errorf("queue: marshal delayed message: %w", err)
+	}
+
+	_, err = c.svc.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:     aws.String(c.queueURL),
+		MessageBody:  aws.String(string(body)),
+		DelaySeconds: int32(delaySec),
+	})
+
+	if err != nil {
+		return fmt.Errorf("queue: send delayed message: %w", err)
+	}
+
+	logger.Log.Info("retry message publised to SQS",
+
+		zap.String("delivery_id", msg.DeliveryID),
+		zap.Int("attempt", msg.Attempt),
+		zap.Int("delay_seconds", delaySec),
+	)
+
+	return nil
+
+}
